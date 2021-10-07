746f9958ff3d478ca7a43710664fdf7c9f687bb1
Mayank Dahiya <dahiyamayank0@gmail.com>
2021-10-07 12:54:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const port = process.env.PORT || 3000;
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+    extended: true
+}))
+
+const Alert = require('./models/schema.js');
+
+
+const d = new Date();
+
+
+app.get('/', (req, res) => {
+    res.render('form');
+})
+
+app.post('/new', (req, res) => {
+    const result = new Alert({
+        FirstName:req.body.fname,
+        LastName:req.body.lname,
+        Gender:req.body.gender,
+        Email:req.body.email,
+        PhoneNumber:req.body.number,
+        Status:req.body.check,
+    })
+    result.save();
+    const mail = req.body.email;
+    const check = req.body.check;
+    const message = {
+        to: mail,
+        from: "harshit0617.cse19@chitkara.edu.in",
+        subject: "Created By Harshit Goyal",
+        html: `<h1>You ${check}  The Office At This Time ${d.getHours()}:${d.getMinutes()} IST</h1>`,
+    }
+
+    sgMail.send(message)
+        .then((response) => {
+            console.log("Sent Successfully");
+        })
+        .catch((err) => {
+            console.log(err);
+        })  
+    res.render('enter');
+})
+
+
+app.post('/enter',(req,res)=>{
+    res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`Connected To Port  ${port}`);
+})
